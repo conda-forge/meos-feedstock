33e0e302a6c6eba4a06164117cdfab7c010703d1
diviloper <vdiviloper@gmail.com>
2024-12-01 18:14:49 +0100

    Update build.sh. Remove no-longer-necessary CMakeLists alteration and use $SHLIB_EXT instead of hardcoded dylib extensions.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,48 +1,23 @@
 #!/bin/bash
 set -e
 
-if [[ "$target_platform" == linux* ]]; then
-    sed -i "s,/usr/local,${PREFIX},g" ./meos/CMakeLists.txt
-    sed -i "s,/opt/homebrew,${PREFIX},g" ./meos/CMakeLists.txt
-elif [[ "$target_platform" == osx* ]]; then
-    sed -i "" "s,/usr/local,${PREFIX},g" ./meos/CMakeLists.txt
-    sed -i "" "s,/opt/homebrew,${PREFIX},g" ./meos/CMakeLists.txt
-fi
-
 mkdir -p build && cd build
 
-if [[ "$target_platform" == linux* ]]; then
-  cmake ${CMAKE_ARGS} \
+cmake ${CMAKE_ARGS} \
         -D CMAKE_BUILD_TYPE=Release \
         -D CMAKE_INSTALL_PREFIX=${PREFIX} \
         -D CMAKE_INSTALL_LIBDIR=lib \
         -D MEOS=on \
         -D GSL_INCLUDE_DIR=${PREFIX}/include \
-        -D GSL_LIBRARY=${PREFIX}/lib/libgsl.so \
-        -D GSL_CBLAS_LIBRARY=${PREFIX}/lib/libgslcblas.so \
+        -D GSL_LIBRARY=${PREFIX}/lib/libgsl${SHLIB_EXT} \
+        -D GSL_CBLAS_LIBRARY=${PREFIX}/lib/libgslcblas${SHLIB_EXT} \
         -D PROJ_INCLUDE_DIRS=${PREFIX}/include \
-        -D PROJ_LIBRARIES=${PREFIX}/lib/libproj.so \
+        -D PROJ_LIBRARIES=${PREFIX}/lib/libproj${SHLIB_EXT} \
         -D GEOS_INCLUDE_DIR=${PREFIX}/include \
-        -D GEOS_LIBRARY=${PREFIX}/lib/libgeos_c.so \
+        -D GEOS_LIBRARY=${PREFIX}/lib/libgeos_c${SHLIB_EXT} \
         -D JSON-C_INCLUDE_DIRS=${PREFIX}/include/json-c \
-        -D JSON-C_LIBRARIES=${PREFIX}/lib/libjson-c.so \
+        -D JSON-C_LIBRARIES=${PREFIX}/lib/libjson-c${SHLIB_EXT} \
         ${SRC_DIR}
-elif [[ "$target_platform" == osx* ]]; then
-  cmake ${CMAKE_ARGS} \
-        -D CMAKE_BUILD_TYPE=Release \
-        -D CMAKE_INSTALL_PREFIX=${PREFIX} \
-        -D CMAKE_INSTALL_LIBDIR=lib \
-        -D MEOS=on \
-        -D GSL_INCLUDE_DIR=${PREFIX}/include \
-        -D GSL_LIBRARY=${PREFIX}/lib/libgsl.dylib \
-        -D GSL_CBLAS_LIBRARY=${PREFIX}/lib/libgslcblas.dylib \
-        -D PROJ_INCLUDE_DIRS=${PREFIX}/include \
-        -D PROJ_LIBRARIES=${PREFIX}/lib/libproj.dylib \
-        -D GEOS_INCLUDE_DIR=${PREFIX}/include \
-        -D GEOS_LIBRARY=${PREFIX}/lib/libgeos_c.dylib \
-        -D JSON-C_INCLUDE_DIRS=${PREFIX}/include/json-c \
-        -D JSON-C_LIBRARIES=${PREFIX}/lib/libjson-c.dylib \
-        ${SRC_DIR}
-fi
+
 make -j
 make install
